f555c308dbb6d1f0a33aca8562898aa22ec746eb
Bruno Mikoski <bruno.mikoski@gmail.com>
2025-02-13 20:31:30 +0000

    add: new favorites feature

---
+++ Scripts/Editor/Core/FavoritesContextMenu.cs
@@ -0,0 +1,98 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace BrunoMikoski.SelectionHistory
+{
+    internal static class FavoritesContextMenu
+    {
+        [MenuItem("Assets/Add to Favorites", false, 2000)]
+        private static void AddToFavoritesAssets()
+        {
+            foreach (Object obj in Selection.objects)
+                FavoritesManager.AddManualFavorite(obj);
+        }
+
+        [MenuItem("Assets/Remove from Favorites", false, 2001)]
+        private static void RemoveFromFavoritesAssets()
+        {
+            foreach (Object obj in Selection.objects)
+                FavoritesManager.RemoveManualFavorite(obj);
+        }
+
+        [MenuItem("Assets/Add to Favorites", true)]
+        private static bool ValidateAddToFavoritesAssets()
+        {
+            foreach (Object obj in Selection.objects)
+                if (FavoritesManager.IsManualFavorite(obj))
+                    return false;
+            return Selection.objects.Length > 0;
+        }
+
+        [MenuItem("Assets/Remove from Favorites", true)]
+        private static bool ValidateRemoveFromFavoritesAssets()
+        {
+            foreach (Object obj in Selection.objects)
+                if (!FavoritesManager.IsManualFavorite(obj))
+                    return false;
+            return Selection.objects.Length > 0;
+        }
+
+        [MenuItem("CONTEXT/Object/Add to Favorites", false, 2000)]
+        private static void AddToFavoritesContext(MenuCommand command)
+        {
+            FavoritesManager.AddManualFavorite(command.context);
+        }
+
+        [MenuItem("CONTEXT/Object/Remove from Favorites", false, 2001)]
+        private static void RemoveFromFavoritesContext(MenuCommand command)
+        {
+            FavoritesManager.RemoveManualFavorite(command.context);
+        }
+
+        [MenuItem("CONTEXT/Object/Add to Favorites", true)]
+        private static bool ValidateAddToFavoritesContext(MenuCommand command)
+        {
+            Object obj = command.context;
+            return obj != null && !FavoritesManager.IsManualFavorite(obj);
+        }
+
+        [MenuItem("CONTEXT/Object/Remove from Favorites", true)]
+        private static bool ValidateRemoveFromFavoritesContext(MenuCommand command)
+        {
+            Object obj = command.context;
+            return obj != null && FavoritesManager.IsManualFavorite(obj);
+        }
+
+        [MenuItem("GameObject/Add to Favorites", false, 2000)]
+        private static void AddToFavoritesGameObject()
+        {
+            foreach (GameObject obj in Selection.gameObjects)
+                FavoritesManager.AddManualFavorite(obj);
+        }
+
+        [MenuItem("GameObject/Remove from Favorites", false, 2001)]
+        private static void RemoveFromFavoritesGameObject()
+        {
+            foreach (GameObject obj in Selection.gameObjects)
+                FavoritesManager.RemoveManualFavorite(obj);
+        }
+
+        [MenuItem("GameObject/Add to Favorites", true)]
+        private static bool ValidateAddToFavoritesGameObject()
+        {
+            foreach (GameObject obj in Selection.gameObjects)
+                if (FavoritesManager.IsManualFavorite(obj))
+                    return false;
+            return Selection.gameObjects.Length > 0;
+        }
+
+        [MenuItem("GameObject/Remove from Favorites", true)]
+        private static bool ValidateRemoveFromFavoritesGameObject()
+        {
+            foreach (GameObject obj in Selection.gameObjects)
+                if (!FavoritesManager.IsManualFavorite(obj))
+                    return false;
+            return Selection.gameObjects.Length > 0;
+        }
+    }
+}
---
+++ Scripts/Editor/Core/FavoritesToolbar.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace BrunoMikoski.SelectionHistory
+{
+    [InitializeOnLoad]
+    internal static class FavoritesToolbar
+    {
+        static FavoritesToolbar()
+        {
+            EditorApplication.delayCall += Initialize;
+        }
+
+        private static void Initialize()
+        {
+            VisualElement parent = new VisualElement
+            {
+                style =
+                {
+                    flexGrow = 0,
+                    flexDirection = FlexDirection.Row
+                }
+            };
+
+            ToolbarMenu favoritesDropdown = new ToolbarMenu
+            {
+                tooltip = "Click to see your favorite assets",
+                text = "Favorites "
+            };
+            ApplyToolbarStyle(favoritesDropdown);
+
+            favoritesDropdown.RegisterCallback<MouseUpEvent>(e =>
+            {
+                if (e.button == 0)
+                    ShowAssetsMenu(favoritesDropdown);
+            });
+
+            parent.Add(favoritesDropdown);
+
+            UnityMainToolbarUtility.AddCustom(UnityMainToolbarUtility.TargetContainer.Left, UnityMainToolbarUtility.Side.Right, parent);
+        }
+
+        private static void ShowAssetsMenu(ToolbarMenu menu)
+        {
+            Rect rect = menu.worldBound;
+            GenericMenu genericMenu = new GenericMenu();
+            List<Object> manual = FavoritesManager.GetManualFavorites();
+            foreach (Object asset in manual)
+            {
+                Object currentAsset = asset;
+                GUIContent content = new GUIContent(currentAsset.name, EditorGUIUtility.ObjectContent(currentAsset, currentAsset.GetType()).image);
+                genericMenu.AddItem(content, false, () =>
+                {
+                    Selection.activeObject = currentAsset;
+                    EditorGUIUtility.PingObject(currentAsset);
+                });
+            }
+            if (manual.Count > 0)
+                genericMenu.AddSeparator("");
+            List<Object> learned = FavoritesManager.GetLearnedFavorites();
+            foreach (Object asset in learned)
+            {
+                if (FavoritesManager.IsManualFavorite(asset))
+                    continue;
+                Object currentAsset = asset;
+                GUIContent content = new GUIContent(currentAsset.name, EditorGUIUtility.ObjectContent(currentAsset, currentAsset.GetType()).image);
+                genericMenu.AddItem(content, false, () =>
+                {
+                    Selection.activeObject = currentAsset;
+                    EditorGUIUtility.PingObject(currentAsset);
+                });
+            }
+            genericMenu.DropDown(rect);
+        }
+
+        private static void ApplyToolbarStyle(VisualElement element)
+        {
+            element.AddToClassList("unity-toolbar-button");
+            element.AddToClassList("unity-editor-toolbar-element");
+            element.RemoveFromClassList("unity-button");
+            element.style.paddingRight = 8;
+            element.style.paddingLeft = 8;
+            element.style.justifyContent = Justify.Center;
+            element.style.display = DisplayStyle.Flex;
+            element.style.borderTopLeftRadius = 2;
+            element.style.borderTopRightRadius = 2;
+            element.style.borderBottomLeftRadius = 2;
+            element.style.borderBottomRightRadius = 2;
+            element.style.height = 19;
+            element.style.marginRight = 1;
+            element.style.marginLeft = 1;
+        }
+    }
+}
